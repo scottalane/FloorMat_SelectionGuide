7f9cc4f7788f62682614b353f7a219f7236a6e2a
Scott Alan Lane <scottalanlane@Scotts-MacBook-Pro-5.local>
2016-03-25 11:12:40 -0700

    friday 3/25 update

--- assets/js.js/script.js
+++ assets/js.js/script.js
@@ -11,10 +11,75 @@ $( document ).ready(function() {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
   })();
-/*
-this is where I need to write my if statements for revealing the clicked on usage area
-information that pops up when a usage area id clicked on
-*/
+  /*this is the timed response prompting the user to Please make a selection-->
+
+
+
+/*this is where I need to write my if statements for revealing
+information that pops up when a usage area is clicked on*/
+
+if (onclick = "entrance"){
+  document.getElementsByTagName(select_entrance);
+}
+
+if (onclick = "cashier"){
+  document.getElementsByTagName(select_cashier);
+}
+  if (onclick = "prep_area"){
+    document.getElementsByTagName(select_prep_area);
+}
+    if (onclick = "bar"){
+      document.getElementsByTagName(select_bar);
+}
+      if (onclick = "cookline"){
+  document.getElementsByTagName(select_cookline);
+}
+        if (onclick = "dishwash_area"){
+          document.getElementsByTagName(select_dishwash_area);
+}
+          if (onclick = "salad_bar"){
+            document.getElementsByTagName(select_salad_bar;
+}
+            if (onclick = "walk_in"){
+              document.getElementsByTagName(select_walk_in);
+}
+              if (onclick = "transitional_area"){
+                document.getElementsByTagName(select_transitional_area);
+                if (onclick = "waitress"){
+                   document.getElementsByTagName(select_waitress);
+                  if (onclick = "restrooms"){
+                     document.getElementsByTagName(select_restrooms);
+                    if (onclick = "transport_mat"){
+                       document.getElementsByTagName(select_transport_mats);
+                      if (onclick = "rubber_mats"){
+                         document.getElementsByTagName(select_rubber_mats);
+                        if (onclick = "carpet_mats"){
+                           document.getElementsByTagName(select_carpet_mats);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+}
+
+
+/*this function make the font-size change*/
+function myFunction() {
+    var str = "Hello World!";
+    var result = str.fontsize(7);
+    document.getElementById("demo").innerHTML = result;
+}
+    
+
+/*this section is what changes the size and color of the clicked on word*/
 document.getElementById("area_selector").onclick = function() {myFunction()};
 
 function myFunction() {
